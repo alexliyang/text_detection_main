ac1e22ed02f6177356548b0f2e263bf596fb4e1a
GuoYi0 <1071221759@qq.com>
2018-03-25 11:05:39 +0800

    add guoyi comment

---
+++ ctpn_tensorflow/lib/datasets/__init__.py
@@ -0,0 +1,24 @@
+from .imdb import imdb
+from .pascal_voc import pascal_voc
+from . import factory
+
+
+# 如果程序的存储路径存在，就
+# 返回的位置
+def _which(program):
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
