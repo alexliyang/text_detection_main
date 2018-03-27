2ab316bd451addf875ebf66170fa5f950902ffa0
cshjarry <314870060@qq.com>
2018-03-27 15:56:46 +0800

    add download module

--- ctpn_new/prepare/download.py
+++ ctpn_new/prepare/download.py
@@ -1,47 +1,86 @@
-# import ftplib
-# import os
-#
-# remote_host = "119.29.161.49"
-#
-pretrain_model_file = "VGG_imagenet.npy"
-# trainset_file = "icpr_text_train.zip"
-#
-#
-# def download():
-#     try:
-#         ftp = ftplib.FTP(remote_host)
-#     except:
-#         print("ERROR cannot reach '%s'" % remote_host)
-#         return
-#     print("..Connected to remote_host '%s'.." % remote_host)
-#
-#     try:
-#         ftp.login()  # 使用匿名账号登陆也就是anonymous
-#     except ftplib.error_perm:
-#         print("ERROR cannot login anonymously")
-#         ftp.quit()
-#         return
-#     print("...logged in as 'anonymously'...")
-#
-#     bufsize = 1024
-#     download_path = os.path.join('dataset/pretrain/')
-#     if not os.path.exists(download_path):
-#         os.makedirs(download_path)
-#
-#     file_handler = open(download_path + pretrain_model_file,'wb').write #以写模式在本地打开文件
-#     ftp.retrbinary('RETR %s' % pretrain_model_file , file_handler, bufsize)  # 接收服务器上文件并写入本地文件
-#
-#     ftp.quit()
-#     return
-#
-#
-# # 调用函数执行测试
-# if __name__ == "__main__":
-#     download()
-
+import hashlib
 import requests
-url = 'https://doc-0o-14-docs.googleusercontent.com/docs/securesc/73al6pct57n4oeq4vf5bq1jgdd34vlee/9o46t1mpu8k1okk09pcq30tv6t2mc6qj/1522108800000/09469196320027156440/09613593125903115016/0B_WmJoEtfQhDYTRvNU9PQmlydlk?e=download&nonce=rkh7crn2dij0k&user=09613593125903115016&hash=ain5kp2r2t1045ofguf7tti9h4dnp3fn'
-r = requests.get(url)
-print('get url successful')
-with open(pretrain_model_file, "wb") as code:
-    code.write(r.content)
+import os
+import zipfile
+import shutil
+
+
+def md5sum(filename):
+    fd = open(filename, "rb")
+    fcont = fd.read()
+    fd.close()
+    fmd5 = hashlib.md5(fcont)
+    return fmd5.hexdigest()
+
+
+def get_file_path(base, file_name):
+    file_path = os.path.join(base, file_name)
+    if not os.path.exists(base):
+        os.makedirs(base)
+
+    return file_path
+
+
+def un_zip(file_name, extract_dir):
+    """unzip zip file"""
+    zip_file = zipfile.ZipFile(file_name)
+    if os.path.isdir(extract_dir):
+        pass
+    else:
+        os.mkdir(extract_dir)
+    for names in zip_file.namelist():
+        zip_file.extract(names, extract_dir)
+    zip_file.close()
+
+
+def main():
+    file_list = [
+        'VGG_imagenet.npy',
+        'icpr_text_train_10000.zip'
+    ]
+    url_list = [
+        'http://ch-10035517.cossh.myqcloud.com/VGG_imagenet.npy',
+        'http://ch-10035517.cossh.myqcloud.com/icpr_text_train_10000.zip'
+    ]
+    base = 'dataset'
+
+    md5 = [
+        '102f510d020773a884e76814e197170f',  # vgg16 md5
+        'e7ea68b7d69b248c98328a590dc82839'
+    ]
+    prefix = ['pretrain/', 'tmp/']
+
+    for ix, url in enumerate(url_list):
+        path = get_file_path(os.path.join(base, prefix[ix]), file_list[ix])
+        if os.path.exists(path) and md5sum(path) == md5[ix]:
+            print('using exits file {}'.format(file_list[ix]))
+        else:
+            print('starting download {}'.format(file_list[ix]))
+            r = requests.get(url)
+            print('download file {} successful'.format(file_list[ix]))
+            with open(path, "wb") as code:
+                code.write(r.content)
+            print('write {} file successful'.format(file_list[ix]))
+        if ix == 1:
+            print('starting extracting train data......')
+            extract_path = get_file_path(base, 'ICPR_text_train')
+            shutil.rmtree(extract_path)
+            os.makedirs(extract_path)
+
+            un_zip(path, extract_path)
+            print('extracting over')
+            img_len = len(os.listdir(extract_path + '/image_10000'))
+            txt_len = len(os.listdir(extract_path + '/text_10000'))
+
+            # print("image size", img_len)
+            # print("text size", txt_len)
+
+            os.rename(extract_path + '/image_10000', extract_path + '/image')
+            os.rename(extract_path + '/text_10000', extract_path + '/text')
+            print('starting cleaning tmp file')
+            os.listdir()
+            shutil.rmtree(os.path.join(extract_path, '__MACOSX'))
+            shutil.rmtree(os.path.join(base, prefix[ix]))
+
+
+main()
